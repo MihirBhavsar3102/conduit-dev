64ace2ebae38ebfc6ac23e5e8b9510f1ee13d502
Ubuntu <ubuntu@ip-172-31-16-247.ap-south-1.compute.internal>
2026-07-14 12:01:21 +0000

    first commit

---
+++ backend/config/config.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'conduit-development', 
+  process.env.DB_USER || 'postgres', 
+  process.env.DB_PASSWORD || 'mihir'
+  {
+    host: process.env.DB_HOST || 'postgres_db', // Crucial: resolves to the docker container name
+    dialect: 'postgres',
+    logging: false
+  }
+);
+
+module.exports = sequelize;
